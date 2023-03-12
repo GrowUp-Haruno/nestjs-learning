455386d95ca009896e2393c74e9d4cdee646bdd4
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2023-03-12 14:44:14 +0900

    postgresに接続

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    ItemsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'postgres',
+      username: 'postgres',
+      password: 'postgres',
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
